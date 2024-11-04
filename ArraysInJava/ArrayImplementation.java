3d07ad0e58ab8d72ab5ace713b2e457c7923a459
sagartiwari0608 <sagartiwarisrt@gmail.com>
2024-11-04 23:05:56 +0530

    added few more methods to custom array and added dynamic spacefeature

--- ArraysInJava/ArrayImplementation.java
+++ ArraysInJava/ArrayImplementation.java
@@ -3,14 +3,20 @@ package ArraysInJava;
 public class ArrayImplementation {
     public static void main(String[] args) {
 
-        MyArrays newArray = new MyArrays(3);
+        MyArrays newArray = new MyArrays(7);
         newArray.insert(4);
         newArray.insert(5);
         newArray.insert(6);
         System.out.println(newArray);
-        System.out.println(newArray.find(5));
-        System.out.println("removed item " + newArray.removeAtIndex(1));
-        System.out.println(newArray.find(5));
+        // System.out.println(newArray.find(5));
+        // System.out.println("removed item " + newArray.removeAtIndex(0));
+        // System.out.println(newArray.find(5));
+        // System.out.println(newArray);
+        // System.out.println("removed item " + newArray.removeAtIndex(10));
+        // System.out.println(newArray.max());
+        // System.out.println(newArray.min());
+        newArray.reverseArray();
+        System.out.println(newArray);
     }
 
 }
--- ArraysInJava/MyArrays.java
+++ ArraysInJava/MyArrays.java
@@ -10,6 +10,16 @@ public class MyArrays {
     }
 
     public void insert(int value) {
+        // this is just for insertion
+        // this.items[index] = value;
+        // this.index++;
+
+        // this will be for incrementing the array size by twice so that we dont
+        // increment it always.
+        if (this.index == this.items.length) {
+            int[] temp = new int[items.length * 2];
+
+        }
         this.items[index] = value;
         this.index++;
     }
@@ -37,10 +47,63 @@ public class MyArrays {
 
     public int removeAtIndex(int indexToDelete) {
         var removedItem = items[indexToDelete];
+        if (indexToDelete > this.index) {
+            throw new IllegalArgumentException();
+        }
         for (int i = indexToDelete; i < this.index - 1; i++) {
             items[i] = items[i + 1];
         }
         this.index--;
+        this.items[index] = 0;
         return removedItem;
+    }
+
+    public int max() {
+        var maxItem = this.items[0];
+        for (int i = 0; i < this.index; i++) {
+            if (this.items[i] > maxItem) {
+                maxItem = this.items[i];
+            }
+        }
+        return maxItem;
+    }
+
+    public int min() {
+        var minItem = this.items[0];
+        for (int i = 0; i < this.index; i++) {
+            if (this.items[i] < minItem) {
+                minItem = this.items[i];
+            }
+        }
+        return minItem;
+    }
+
+    public void reverseArray() {
+        // this method works but isn't optimal as it was known it takes O(n) space and
+        // time both.
+        // One Point to note is that when we are asked to not change the original array
+        // then we are supposed to use O(n) space complexity.
+        // except time complexity should always be O(1) with the TWO POINTER METHOD.
+
+        // var reversedArray = new int[this.index];
+        // var tempIndex = 0;
+        // for (int i = this.index; i >= 0; i--) {
+        // reversedArray[tempIndex] = this.items[i];
+        // System.out.println(reversedArray[tempIndex]);
+        // }
+
+        // Two Pointer method.
+        int i = 0;
+        int j = this.index - 1;
+        var temp = 0;
+
+        while (i < j) {
+            temp = this.items[i];
+            this.items[i] = this.items[j];
+            this.items[j] = temp;
+            i++;
+            j--;
+        }
+
     }
 }
