9a91fac17db502848e162ad3e5886bbc4d34495a
sagartiwari0608 <sagartiwarisrt@gmail.com>
2024-11-21 23:15:06 +0530

    intesection of two arrays the better way and worse way

--- LeetCodeQuestionsSolved/IntersectionOfTwoArrays.java
+++ LeetCodeQuestionsSolved/IntersectionOfTwoArrays.java
@@ -1,11 +1,50 @@
 package LeetCodeQuestionsSolved;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 
 public class IntersectionOfTwoArrays {
 
-Set<Integer> mySet = new HashSet<>(Arrays.asList({4,5,9}));
+    public int[] intersection(int[] nums1, int[] nums2) {
 
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> resultSet = new HashSet<>();
+        for (int i : nums1)
+            set.add(i);
+        for (int j : nums1) {
+            if (set.contains(j)) {
+                resultSet.add(j);
+            }
+        }
+        int[] resultArr = new int[resultSet.size()];
+        int index = 0;
+        for (Integer x : resultSet) {
+            resultArr[index++] = x;
+        }
+        return resultArr;
+
+        // the following is the way used by piyush in his videos. and it works but i was
+        // stupid so it didn't work for me . anyways there is a better solution with
+        // lesser time complexity.
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // while (i < nums1.length && j < nums2.length) {
+        // System.out.println(nums1[i] + " " + nums2[j]);
+        // if (nums1[i] == nums2[j]) {
+        // set.add(nums1[i]);
+        // i++;
+        // j++;
+        // } else if (nums1[i] < nums2[j]) {
+        // i++;
+        // } else {
+        // j++;
+        // }
+        // }
+        // int[] nums3 = new int[set.size()];
+        // int index = 0;
+        // for (var x : set) {
+        // nums3[index] = Integer.parseInt(x.toString());
+        // index++;
+        // }
+        // return nums3;
+    }
 }
