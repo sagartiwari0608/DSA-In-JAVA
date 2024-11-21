7ab767fb4f77f83f6ff1c81c1ea85d4504c08204
sagartiwari0608 <sagartiwarisrt@gmail.com>
2024-11-21 19:58:04 +0530

    sort colors finally did the other solution

---
+++ LeetCodeQuestionsSolved/IntersectionOfTwoArrays.java
@@ -0,0 +1,11 @@
+package LeetCodeQuestionsSolved;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+
+Set<Integer> mySet = new HashSet<>(Arrays.asList({4,5,9}));
+
+}
--- LeetCodeQuestionsSolved/SortColors.java
+++ LeetCodeQuestionsSolved/SortColors.java
@@ -3,34 +3,68 @@ package LeetCodeQuestionsSolved;
 public class SortColors {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.sortColors(new int[] { 1, 0, 2, 1, 0, 0 }));
+        SortColorsImplementation sol = new SortColorsImplementation();
+        System.out.println(sol.sortColors(new int[] { 2, 0, 2, 1, 1, 0 }));
     }
 
 }
 
-class Solution {
+class SortColorsImplementation {
     public int[] sortColors(int[] nums) {
-        int low = 0;
-        int high = nums.length - 1;
-        int i = 0;
-        while (low < high && i <= high) {
-            if (nums[i] == 1) {
-                i++;
-            } else if (nums[i] == 0) {
-                int temp = nums[i];
-                nums[i] = nums[low];
-                nums[low] = temp;
-                low++;
-                i++;
-            } else if (nums[i] == 2) {
-                int temp = nums[i];
-                nums[i] = nums[high];
-                nums[high] = temp;
-                high--;
-                i++;
-            }
+        // Couting method. count all the zeros ones and twos then add them in same array
+        // but in order.
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        int index = 0;
+
+        for (int i : nums) {
+            if (i == 0)
+                zeros++;
+            else if (i == 1)
+                ones++;
+            else if (i == 2)
+                twos++;
         }
+        while (zeros > 0) {
+            nums[index] = 0;
+            index++;
+            zeros--;
+        }
+        while (ones > 0) {
+            nums[index] = 1;
+            index++;
+            ones--;
+        }
+        while (twos > 0) {
+            nums[index] = 2;
+            index++;
+            twos--;
+        }
+
+        // This following solution is logically good but fails in certain cases for
+        // example [1,2,0] this one. here it returns [1,0,2] hence we need to use the
+        // other couting and adding method.
+        // int low = 0;
+        // int high = nums.length - 1;
+        // int i = 0;
+        // while (low < high && i <= high) {
+        // if (nums[i] == 1) {
+        // i++;
+        // } else if (nums[i] == 0) {
+        // int temp = nums[i];
+        // nums[i] = nums[low];
+        // nums[low] = temp;
+        // low++;
+        // i++;
+        // } else if (nums[i] == 2) {
+        // int temp = nums[i];
+        // nums[i] = nums[high];
+        // nums[high] = temp;
+        // high--;
+        // i++;
+        // }
+        // }
         return nums;
     }
 }
